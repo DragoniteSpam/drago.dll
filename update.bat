430415f282d40996b7064686bbca7ee9ca9cc07f
Dragonite <mpeng@optimum.net>
2021-03-09 20:49:29 -0500

    update batch file copies the dll to the test project as well

--- update.bat
+++ update.bat
@@ -1,2 +1,3 @@
 cd "C:\GameMakerStudio2\DDD Workspace\drago.dll"
 copy "C:\GameMakerStudio2\DDD Workspace\drago.dll\Debug\drago.dll" "C:\GameMakerStudio2\DDDEditor2-23\datafiles\data\drago.dll"
+copy "C:\GameMakerStudio2\DDD Workspace\drago.dll\Debug\drago.dll" "%USERPROFILE%\Documents\GameMakerStudio2\Sprite Packing Test\datafiles\drago.dll"
