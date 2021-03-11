f3757fe1f50cbd59538a4cfa80cc9c9991efde1f
Dragonite <mpeng@optimum.net>
2021-03-10 22:42:18 -0500

    update the updater

--- main/update.bat
+++ main/update.bat
@@ -1,3 +1,3 @@
-cd "C:\GameMakerStudio2\DDD Workspace\drago.dll"
-copy "C:\GameMakerStudio2\DDD Workspace\drago.dll\Debug\drago.dll" "C:\GameMakerStudio2\DDDEditor2-23\datafiles\data\drago.dll"
-copy "C:\GameMakerStudio2\DDD Workspace\drago.dll\Debug\drago.dll" "%USERPROFILE%\Documents\GameMakerStudio2\Sprite Packing Test\datafiles\drago.dll"
+cd "C:\GameMakerStudio2\DDD Workspace\DLL Core\main"
+copy "C:\GameMakerStudio2\DDD Workspace\DLL Core\main\Debug\drago.dll" "C:\GameMakerStudio2\DDDEditor2-23\datafiles\data\drago.dll"
+copy "C:\GameMakerStudio2\DDD Workspace\DLL Core\main\Debug\drago.dll" "%USERPROFILE%\Documents\GameMakerStudio2\Sprite Packing Test\datafiles\drago.dll"
