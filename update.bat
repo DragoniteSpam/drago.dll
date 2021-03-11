4a581b1bde5482da50394a53459d0567a56b293b
Dragonite <mpeng@optimum.net>
2021-03-11 12:26:48 -0500

    updater includes the file dropper

--- update.bat
+++ update.bat
@@ -1,3 +1,4 @@
 cd "C:\GameMakerStudio2\DDD Workspace\DLL Core\main"
 copy "C:\GameMakerStudio2\DDD Workspace\DLL Core\main\Debug\drago.dll" "C:\GameMakerStudio2\DDDEditor2-23\datafiles\data\drago.dll"
 copy "C:\GameMakerStudio2\DDD Workspace\DLL Core\main\Debug\SpriteAtlas.dll" "%USERPROFILE%\Documents\GameMakerStudio2\Sprite Packing Test\datafiles\SpriteAtlas.dll"
+copy "C:\GameMakerStudio2\DDD Workspace\DLL Core\main\Debug\FileDropper.dll" "%USERPROFILE%\Documents\GameMakerStudio2\File Dropper 23\datafiles\FileDropper.dll"
