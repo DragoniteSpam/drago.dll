1b2abe060574ef5c9224354ff5ecbe42fac5f1dd
Dragonite <mpeng@optimum.net>
2021-03-11 09:56:41 -0500

    implemented a versioning note in spriteatlas (doesnt compile)

--- SpriteAtlas/dllmain.cpp
+++ SpriteAtlas/dllmain.cpp
@@ -1,10 +1,13 @@
 #include "spriteatlas.h"
 
 ex double init() {
-    
     return 1.0;
 }
 
 ex double pack_textures(int* sprite_data, double length) {
     return sprite_atlas::pack(sprite_data, ((int)length) >> 2);
+}
+
+ex const char* version() {
+    return sprite_atlas::version();
 }
--- SpriteAtlas/spriteatlas.h
+++ SpriteAtlas/spriteatlas.h
@@ -1,6 +1,11 @@
 #include "main/core.h"
 
 namespace sprite_atlas {
+	const char* VERSION = "1.0.1";
+	const char* version() {
+		return VERSION;
+	}
+
 	double pack(int*, int);
 	bool place_sprite(int*, int, int, int, int);
 	bool collide_sprite(int*, int, int, int, int);
