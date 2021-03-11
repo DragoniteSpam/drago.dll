ec9e6d3211d4870bd672a548f7872cb6fc660f04
Dragonite <mpeng@optimum.net>
2021-03-11 10:12:30 -0500

    renamed for consistency

--- SpriteAtlas/dllmain.cpp
+++ SpriteAtlas/dllmain.cpp
@@ -4,10 +4,10 @@ ex double init() {
     return 1.0;
 }
 
-ex double sprite_pack_textures(int* sprite_data, double length) {
+ex double sprite_atlas_pack(int* sprite_data, double length) {
     return sprite_atlas::pack(sprite_data, ((int)length) >> 2);
 }
 
-ex const char* sprite_pack_version() {
+ex const char* sprite_atlas_version() {
     return sprite_atlas::version();
 }
