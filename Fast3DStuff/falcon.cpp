90f51d88e24b8c542a38452a703e06bf463aaca8
Dragonite <mpeng@optimum.net>
2021-03-19 01:20:16 -0400

    falcon: added a combine color function, which multiplies the vertex color

--- Fast3DStuff/falcon.cpp
+++ Fast3DStuff/falcon.cpp
@@ -2,6 +2,7 @@
 
 namespace falcon {
 	int vertex_size = 9;
+	int vertex_color_offset = 8;
 
 	const char* version() {
 		return __DRAGO_FALCON;
@@ -20,6 +21,22 @@ namespace falcon {
 				combine_target[index + i + 0] += combine_positions[tree * FALCON_BATCH_DATA_SIZE + 1];
 				combine_target[index + i + 1] += combine_positions[tree * FALCON_BATCH_DATA_SIZE + 2];
 				combine_target[index + i + 2] += combine_positions[tree * FALCON_BATCH_DATA_SIZE + 3];
+			}
+			index += tree_size;
+		}
+	}
+
+	void combine_color(float* combine_target, float* combine_positions, double count) {
+		int tree_count = (int)count;
+		int index = 0;
+		for (int tree = 0; tree < tree_count; tree++) {
+			int tree_size = ((int*)(combine_positions))[tree * FALCON_BATCH_DATA_SIZE + 0] / 4;
+			for (int i = 0; i < tree_size; i += vertex_size) {
+				combine_target[index + i + 0] += combine_positions[tree * FALCON_BATCH_DATA_SIZE + 1];
+				combine_target[index + i + 1] += combine_positions[tree * FALCON_BATCH_DATA_SIZE + 2];
+				combine_target[index + i + 2] += combine_positions[tree * FALCON_BATCH_DATA_SIZE + 3];
+
+				combine_target[index + i + vertex_color_offset] *= combine_positions[tree * FALCON_BATCH_DATA_SIZE + vertex_color_offset];
 			}
 			index += tree_size;
 		}
--- Fast3DStuff/falcon.h
+++ Fast3DStuff/falcon.h
@@ -2,15 +2,19 @@
 #define __DRAGO_FALCON "1.0.2"
 
 #define FALCON_BATCH_DATA_SIZE 4
+#define FALCON_BATCH_DATA_SIZE_COLOR 5
 
 #include "falcon.h"
 
 namespace falcon {
 	extern int vertex_size;
+	extern int vertex_color_offset;
 
 	const char* version();
 	void set_vertex_size(int);
 	void combine(float*, float*, double);
+
+	void combine_color(float*, float*, double);
 }
 
 #endif
