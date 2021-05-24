884ca5ad15c9525e129b4b2e504c32122454bfe4
Michael <mpeng@optimum.net>
2021-05-24 15:31:58 -0400

    initialize the world

--- Bullet/bulletgml.cpp
+++ Bullet/bulletgml.cpp
@@ -4,4 +4,7 @@ namespace bullet {
 	const char* version() {
 		return __DRAGO_BULLET;
 	}
+	int get_world_count() {
+		return worlds.size();
+	}
 }
--- Bullet/bulletgml.h
+++ Bullet/bulletgml.h
@@ -2,9 +2,19 @@
 #define __DRAGO_BULLET "0.0.1"
 
 #include "bullet/btBulletDynamicsCommon.h"
+#include <vector>
 
 namespace bullet {
+	class BulletInstance {
+	public:
+	private:
+		btDiscreteDynamicsWorld world;
+	};
+	
 	const char* version();
+	int get_world_count();
+
+	std::vector<bullet::BulletInstance> worlds;
 }
 
 #endif
