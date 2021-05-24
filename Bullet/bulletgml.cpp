a1bd7371f6bfdb2861f87025f4331fa90d7beade
Michael <mpeng@optimum.net>
2021-05-24 14:40:43 -0400

    bullet setup

---
+++ Bullet/bulletgml.cpp
@@ -0,0 +1,7 @@
+#include "bulletgml.h"
+
+namespace bullet {
+	const char* version() {
+		return __DRAGO_BULLET;
+	}
+}
---
+++ Bullet/bulletgml.h
@@ -0,0 +1,8 @@
+#ifndef __DRAGO_BULLET
+#define __DRAGO_BULLET "0.0.1"
+
+namespace bullet {
+	const char* version();
+}
+
+#endif
