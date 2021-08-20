5feb4420d279dd88789dbd66cc2a4873ff15547d
Michael <mpeng@optimum.net>
2021-08-20 13:25:20 -0400

    macaw cpp outline

---
+++ Macaw/macaw.cpp
@@ -0,0 +1,19 @@
+#include "macaw.h"
+
+namespace macaw {
+	const char* version() {
+		return __DRAGO_MACAW;
+	}
+
+	void generate(int w, int h, int octaves) {
+
+	}
+
+	float[][] _gen_white_noise(int w, int h) {
+		return 0;
+	}
+
+	float[][] _gen_smooth_noise(float[][] base_noise, int octave) {
+		return 0;
+	}
+}
