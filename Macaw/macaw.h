1124550ae86bc088cc403302d1885aff6f854b23
Michael <mpeng@optimum.net>
2021-08-20 13:21:59 -0400

    macaw header and namespace

--- Macaw/macaw.h
+++ Macaw/macaw.h
@@ -1,4 +1,14 @@
 #ifndef __DRAGO_MACAW
 #define __DRAGO_MACAW "0.0.1"
 
+#include "main/core.h"
+
+namespace macaw {
+	const char* version();
+
+	void generate(int, int, int);
+	float[][] _gen_white_noise(int, int);
+	float[][] _gen_smooth_noise(float[][], int);
+}
+
 #endif
