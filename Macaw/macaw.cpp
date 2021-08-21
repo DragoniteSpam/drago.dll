6f2e0c7fea2993c4abd329d3eaecf4ed5c954f89
Michael <mpeng@optimum.net>
2021-08-20 23:28:37 -0400

    gen_smooth_noise does the entire array at once

--- Macaw/macaw.cpp
+++ Macaw/macaw.cpp
@@ -2,6 +2,8 @@
 
 #include "macaw.h"
 
+#define GET_SMOOTH_INDEX(i, j, octave) ((i + j) * octave + i * h + j)
+
 namespace macaw {
 	const char* version() {
 		return __DRAGO_MACAW;
@@ -31,35 +33,37 @@ namespace macaw {
 		return noise;
 	}
 
-	float* _gen_smooth_noise(float* base_noise, int w, int h, int octave) {
-		int len = w * h;
+	float* _gen_smooth_noise(float* base_noise, int w, int h, int octaves) {
+		int len = w * h * octaves;
 		float* smooth = new float[len];
 
-		int period = 1 << octave;
-		float frequency = 1.0 / period;
+		for (int octave = 0; octave < octaves; octave++) {
+			int period = 1 << octave;
+			float frequency = 1.0 / period;
 
-		for (int i = 0; i < w; i++) {
-			for (int j = 0; j < h; j++) {
-				int i0 = (i / period) * period;
-				int i1 = (i0 + period) % w;
-				float hblend = (i - i0) * frequency;
-
+			for (int i = 0; i < w; i++) {
 				for (int j = 0; j < h; j++) {
-					int j0 = (j / period) * period;
-					int j1 = (j0 + period) % h;
-					float vblend = (j - j0) * frequency;
+					int i0 = (i / period) * period;
+					int i1 = (i0 + period) % w;
+					float hblend = (i - i0) * frequency;
 
-					float b00 = base_noise[i0 * h + j0];
-					float b01 = base_noise[i0 * h + j1];
-					float b10 = base_noise[i1 * h + j0];
-					float b11 = base_noise[i1 * h + j1];
+					for (int j = 0; j < h; j++) {
+						int j0 = (j / period) * period;
+						int j1 = (j0 + period) % h;
+						float vblend = (j - j0) * frequency;
 
-					float top = interpolate(b00, b10, hblend);
-					float bottom = interpolate(b01, b11, hblend);
+						float b00 = base_noise[i0 * h + j0];
+						float b01 = base_noise[i0 * h + j1];
+						float b10 = base_noise[i1 * h + j0];
+						float b11 = base_noise[i1 * h + j1];
+
+						float top = interpolate(b00, b10, hblend);
+						float bottom = interpolate(b01, b11, hblend);
 
 #pragma warning(disable:6386)
-					smooth[i * h + j] = interpolate(top, bottom, vblend);
+						smooth[GET_SMOOTH_INDEX(i, j, octave)] = interpolate(top, bottom, vblend);
 #pragma warning(default:6386)
+					}
 				}
 			}
 		}
@@ -75,3 +79,5 @@ namespace macaw {
 		return a * (1 - f) + f * b;
 	}
 }
+
+#undef GET_SMOOTH_INDEX
