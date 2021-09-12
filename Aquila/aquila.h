e5a1116f079d45451f7d4b3fd07e98188b947ca4
Michael <mpeng@optimum.net>
2021-09-12 12:16:38 -0400

    graph and node classes

--- Aquila/aquila.h
+++ Aquila/aquila.h
@@ -1,6 +1,7 @@
 #ifndef __DRAGO_AQUILA
 #define __DRAGO_AQUILA "0.0.1"
 
+#include <map>
 #include "main/core.h"
 
 namespace aquila {
@@ -12,6 +13,30 @@ namespace aquila {
 	void disconnect_nodes(int, int);
 	void clear_nodes();
 	void navigate();
+
+	class Graph {
+	private:
+		std::map<int, Node> nodes;
+		int id;
+	public:
+		Graph();
+		int AddNode();
+		void RemoveNode(Node* node);
+		void ConnectNodes(Node* a, Node* b);
+		void DisconnectNodes(Node* a, Node* b);
+		void ClearNodes();
+		void Navigate(Node* source, Node* destination);
+	};
+
+	class Node {
+	private:
+		int id;
+	public:
+		Node(Graph* graph);
+		std::map<int, Node>* GetConnections();
+		void Connect(Node* node);
+		void Disconnect(Node* node);
+	};
 }
 
 #endif
