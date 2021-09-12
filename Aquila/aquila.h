a7992ec90e7a758040068640dc07092dc8e6b4e5
Michael <mpeng@optimum.net>
2021-09-12 12:00:53 -0400

    aquila namespace definition

--- Aquila/aquila.h
+++ Aquila/aquila.h
@@ -3,4 +3,15 @@
 
 #include "main/core.h"
 
+namespace aquila {
+	const char* version();
+
+	void add_node(int);
+	void remove_node(int);
+	void connect_nodes(int, int);
+	void disconnect_nodes(int, int);
+	void clear_nodes();
+	void navigate();
+}
+
 #endif
