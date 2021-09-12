1b27fe289c518cea792015236cde886f690f03a1
Michael <mpeng@optimum.net>
2021-09-12 12:03:44 -0400

    aquila.cpp

---
+++ Aquila/aquila.cpp
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+#include "aquila.h"
+
+namespace aquila {
+	const char* version() {
+		return __DRAGO_AQUILA;
+	}
+
+	void add_node(int id) {
+
+	}
+
+	void remove_node(int id) {
+
+	}
+
+	void connect_nodes(int a, int b) {
+
+	}
+
+	void disconnect_nodes(int a, int b) {
+
+	}
+
+	void clear_nodes() {
+
+	}
+
+	void navigate() {
+
+	}
+}
