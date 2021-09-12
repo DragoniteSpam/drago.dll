222ddce9756be26609ca27a784a8439e7df10143
Michael <mpeng@optimum.net>
2021-09-12 11:57:50 -0400

    outline functions in aquila/dllmain

--- Aquila/dllmain.cpp
+++ Aquila/dllmain.cpp
@@ -1,1 +1,34 @@
 #include "aquila.h"
+
+ex const char* aquila_version() {
+	return __DRAGO_AQUILA;
+}
+
+ex double aquila_add_node(double index) {
+	
+	return 1.0;
+}
+
+ex double aquila_remove_node(double index) {
+	
+	return 1.0;
+}
+
+ex double aquila_connect_nodes(double a, double b) {
+	
+	return 1.0;
+}
+
+ex double aquila_disconnect_nodes(double a, double b) {
+	
+	return 1.0;
+}
+
+ex double aquila_clear_nodes() {
+
+	return 1.0;
+}
+
+ex double aquila_navigate(double a, double b) {
+	return 1.0;
+}
