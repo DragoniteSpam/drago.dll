9c2659edebcad474ca40ac1680f95c8c9747e3f1
Michael <mpeng@optimum.net>
2022-01-06 17:13:05 -0500

    spriteops outline

--- SpriteOperations/dllmain.cpp
+++ SpriteOperations/dllmain.cpp
@@ -1,2 +1,14 @@
 #include "main/core.h"
 #include "spriteops.h"
+
+ex const char* spriteops_version() {
+
+}
+
+ex double spriteops_get_cropped_dimensions(float* data, double length, double* out) {
+
+}
+
+ex double spriteops_remove_transparent_colour(float* data, double length, double colour) {
+
+}
