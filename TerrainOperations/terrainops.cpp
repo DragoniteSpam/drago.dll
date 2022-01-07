2e70124709502b7d7b84074999ccb6eb7d63a64c
Michael <mpeng@optimum.net>
2022-01-06 23:22:17 -0500

    implemented terrainops to_heightmap

--- TerrainOperations/terrainops.cpp
+++ TerrainOperations/terrainops.cpp
@@ -3,7 +3,11 @@
 
 namespace terrainops {
 	void to_heightmap(float* data, unsigned int* out, int len, double scale) {
-
+		int z;
+		for (int i = 0; i < len; i++) {
+			z = std::clamp((int)(data[i] * scale), 0, 255);
+			out[i] = 0xff000000 | (z | (z << 8) | (z << 16));
+		}
 	}
 
 	void from_heightmap(float* data, unsigned int* in, int len, double scale) {
