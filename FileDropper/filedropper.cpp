931f6ac9835853551dced9789abb28d4ad2cd6ac
Michael <mpeng@optimum.net>
2022-01-11 16:23:37 -0500

    Revert "file dropper doesnt need to keep track of the original window pointer, at least for now"
    This reverts commit 9fe33834467bcca40e83d1bb1101fb3d4277b827.

--- FileDropper/filedropper.cpp
+++ FileDropper/filedropper.cpp
@@ -1,9 +1,11 @@
 #include "filedropper.h"
 
 namespace file_dropper {
+    LONG_PTR window_original;
     std::vector<std::string> names;
 
     void init(HWND hWnd) {
+        window_original = GetWindowLongPtr(hWnd, GWLP_WNDPROC);
         DragAcceptFiles(hWnd, true);
     }
     
