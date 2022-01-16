0c0f9d823729e644ecb16686bf923336bafad6fa
Michael <mpeng@optimum.net>
2022-01-16 01:57:07 -0500

    aquila api

--- Aquila/Aquila.cs
+++ Aquila/Aquila.cs
@@ -3,10 +3,38 @@ using System;
 using System.Collections.Generic;
 
 namespace Aquila {
-    public class Aquila {
+    public class GML {
         [DllExport]
         public static string Version() {
             return "0.0.0";
+        }
+        [DllExport]
+        public static double AddNode(double graph, double data) {
+            return 1;
+        }
+        [DllExport]
+        public static double RemoveNode(double graph, double data) {
+            return 1;
+        }
+        [DllExport]
+        public static double ConnectNode(double graph, double a, double b) {
+            return 1;
+        }
+        [DllExport]
+        public static double DisconnectNodes(double graph, double a, double b) {
+            return 1;
+        }
+        [DllExport]
+        public static double ClearNodes(double graph) {
+            return 1;
+        }
+        [DllExport]
+        public static double Size(double graph) {
+            return 1;
+        }
+        [DllExport]
+        public static double Navigate(double source, double destination) {
+            return 1;
         }
     }
 }
