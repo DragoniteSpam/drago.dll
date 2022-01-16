6499bf2ae43baec0041f6d9ade34c3e091454568
Michael <mpeng@optimum.net>
2022-01-16 02:19:34 -0500

    basic aquila add/remove/connect

--- Aquila/Aquila.cs
+++ Aquila/Aquila.cs
@@ -51,8 +51,22 @@ namespace Aquila {
 
         public int ID { get; private set; }
 
+        private Dictionary<int, Node> nodes = new Dictionary<int, Node>();
+
         public Aquila() {
             ID = id_record++;
+        }
+
+        public int AddNode(double data) {
+            Node addition = new Node(data);
+            nodes[addition.ID] = addition;
+            return addition.ID;
+        }
+
+        public void RemoveNode(int id) {
+            if (nodes.ContainsKey(id)) {
+                nodes.Remove(id);
+            }
         }
     }
 
@@ -60,9 +74,25 @@ namespace Aquila {
         static int id_record = 0;
 
         public int ID { get; private set; }
+        public double Data { get; set; }
 
-        public Node() {
+        private HashSet<Node> connections = new HashSet<Node>();
+
+        public Node(double data) {
             ID = id_record++;
+            Data = data;
+        }
+
+        public void Connect(Node other) {
+            if (!connections.Contains(other)) {
+                connections.Add(other);
+                other.connections.Add(this);
+            }
+        }
+
+        public void Disconnect(Node other) {
+            connections.Remove(other);
+            other.connections.Remove(this);
         }
     }
 }
