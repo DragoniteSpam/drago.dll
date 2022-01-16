2ef77e00e85738604469c7ca0896bba2b126f1a7
Michael <mpeng@optimum.net>
2022-01-16 01:52:17 -0500

    basic dll export

--- Aquila/Aquila.cs
+++ Aquila/Aquila.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using RGiesecke.DllExport;
+using System;
 using System.Collections.Generic;
 
 namespace Aquila {
-    
+    public class Aquila {
+        [DllExport]
+        public static string Version() {
+            return "0.0.0";
+        }
+    }
 }
