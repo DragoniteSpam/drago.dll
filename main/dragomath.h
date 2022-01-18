76a294eb4b786d02d37dd8093fd2b4f7260d2fed
Michael <mpeng@optimum.net>
2022-01-18 00:41:32 -0500

    added BYTES2DOUBLES and DOUBLES2BYTES macros

--- main/dragomath.h
+++ main/dragomath.h
@@ -4,6 +4,8 @@
 // helpers
 #define BYTES2FLOATS(bytes) (((int)(bytes)) / 4)
 #define FLOATS2BYTES(floats) (((int)(floats)) * 4)
+#define BYTES2DOUBLES(bytes) (((int)(bytes)) / 8)
+#define DOUBLES2BYTES(floats) (((int)(floats)) * 8)
 
 #define DOT(vec3) (vec3.x * vec3.x + vec3.y * vec3.y + vec3.z * vec3.z)
 #define MAGNITUDE(vec3) sqrtf(DOT(vec3))
