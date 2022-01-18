126806393db18fba382c86cc7cc88b600e18e7d2
Michael <mpeng@optimum.net>
2022-01-17 21:52:10 -0500

    use locals instead of accessing globals a million times

--- MeshOperations/meshops.cpp
+++ MeshOperations/meshops.cpp
@@ -18,7 +18,8 @@ namespace meshops {
 	void get_bounds(float* data, float* out, int len) {
 		float minx = 10000000, miny = 10000000, minz = 10000000;
 		float maxx = -10000000, maxy = -10000000, maxz = -10000000;
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		int vsize = meshops::vertex_size;
+		for (int i = 0; i < len; i += vsize) {
 			minx = std::min(minx, data[i + 0]);
 			miny = std::min(miny, data[i + 1]);
 			minz = std::min(minz, data[i + 2]);
@@ -42,26 +43,31 @@ namespace meshops {
 	}
 
 	void transform_position(float* data, int len) {
-		for (int i = 0; i < len; i += meshops::vertex_size) {
-			data[i + 0] += meshops::trans_x;
-			data[i + 1] += meshops::trans_y;
-			data[i + 2] += meshops::trans_z;
+		int vsize = meshops::vertex_size;
+		float tx = meshops::trans_x;
+		float ty = meshops::trans_y;
+		float tz = meshops::trans_z;
+		for (int i = 0; i < len; i += vsize) {
+			data[i + 0] += tx;
+			data[i + 1] += ty;
+			data[i + 2] += tz;
 		}
 	}
 
 	void transform_center(float* data, int len) {
+		int vsize = meshops::vertex_size;
 		float total_x = 0;
 		float total_y = 0;
 
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		for (int i = 0; i < len; i += vsize) {
 			total_x += data[i + 0];
 			total_y += data[i + 1];
 		}
 
-		total_x /= len / meshops::vertex_size;
-		total_y /= len / meshops::vertex_size;
+		total_x /= len / vsize;
+		total_y /= len / vsize;
 
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		for (int i = 0; i < len; i += vsize) {
 			data[i + 0] -= total_x;
 			data[i + 1] -= total_y;
 		}
@@ -69,7 +75,8 @@ namespace meshops {
 
 	// could do these with matrices but for set axes i'm pretty sure this is faster
 	void transform_rotate_x(float* data, int len, float angle) {
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		int vsize = meshops::vertex_size;
+		for (int i = 0; i < len; i += vsize) {
 			data[i + 1] = data[i + 1] * cosf(angle) - data[i + 2] * sinf(angle);
 			data[i + 2] = data[i + 1] * sinf(angle) - data[i + 2] * cosf(angle);
 			data[i + 4] = data[i + 4] * cosf(angle) - data[i + 5] * sinf(angle);
@@ -78,7 +85,8 @@ namespace meshops {
 	}
 
 	void transform_rotate_y(float* data, int len, float angle) {
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		int vsize = meshops::vertex_size;
+		for (int i = 0; i < len; i += vsize) {
 			data[i + 0] = data[i + 2] * sinf(angle) - data[i + 0] * cosf(angle);
 			data[i + 2] = data[i + 2] * cosf(angle) - data[i + 0] * sinf(angle);
 			data[i + 3] = data[i + 5] * sinf(angle) - data[i + 3] * cosf(angle);
@@ -87,7 +95,8 @@ namespace meshops {
 	}
 
 	void transform_rotate_z(float* data, int len, float angle) {
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		int vsize = meshops::vertex_size;
+		for (int i = 0; i < len; i += vsize) {
 			data[i + 0] = data[i + 0] * cosf(angle) - data[i + 1] * sinf(angle);
 			data[i + 1] = data[i + 0] * sinf(angle) - data[i + 1] * cosf(angle);
 			data[i + 3] = data[i + 3] * cosf(angle) - data[i + 4] * sinf(angle);
@@ -96,7 +105,8 @@ namespace meshops {
 	}
 
 	void transform_scale(float* data, int len, float scale) {
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		int vsize = meshops::vertex_size;
+		for (int i = 0; i < len; i += vsize) {
 			data[i + 0] *= scale;
 			data[i + 1] *= scale;
 			data[i + 2] *= scale;
@@ -106,8 +116,9 @@ namespace meshops {
 	// axes
 	void rotate_up(float* data, int len) {
 		float x, y, z, nx, ny, nz;
+		int vsize = meshops::vertex_size;
 
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		for (int i = 0; i < len; i += vsize) {
 			x = data[i + 0];
 			y = data[i + 1];
 			z = data[i + 2];
@@ -125,115 +136,127 @@ namespace meshops {
 
 	void mirror_axis_x(float* data, int len) {
 		float x1, x2, x3, y1, y2, y3, z1, z2, z3;
+		int vsize = meshops::vertex_size;
+		int iteration = vsize * 3;
 
-		for (int i = 0; i < len; i += meshops::vertex_size * 3) {
+		for (int i = 0; i < len; i += iteration) {
 			x1 = data[i + 0];
 			y1 = data[i + 1];
 			z1 = data[i + 2];
-			x2 = data[i + 0 + meshops::vertex_size];
-			y2 = data[i + 1 + meshops::vertex_size];
-			z2 = data[i + 2 + meshops::vertex_size];
-			x3 = data[i + 0 + meshops::vertex_size * 2];
-			y3 = data[i + 1 + meshops::vertex_size * 2];
-			z3 = data[i + 2 + meshops::vertex_size * 2];
+			x2 = data[i + 0 + vsize];
+			y2 = data[i + 1 + vsize];
+			z2 = data[i + 2 + vsize];
+			x3 = data[i + 0 + vsize * 2];
+			y3 = data[i + 1 + vsize * 2];
+			z3 = data[i + 2 + vsize * 2];
 			data[i + 0] = -x1;
 			data[i + 1] = -y1;
 			data[i + 2] = -z1;
-			data[i + 0 + meshops::vertex_size] = -x3;
-			data[i + 1 + meshops::vertex_size] = -y3;
-			data[i + 2 + meshops::vertex_size] = -z3;
-			data[i + 0 + meshops::vertex_size * 2] = -x2;
-			data[i + 1 + meshops::vertex_size * 2] = -y2;
-			data[i + 2 + meshops::vertex_size * 2] = -z2;
+			data[i + 0 + vsize] = -x3;
+			data[i + 1 + vsize] = -y3;
+			data[i + 2 + vsize] = -z3;
+			data[i + 0 + vsize * 2] = -x2;
+			data[i + 1 + vsize * 2] = -y2;
+			data[i + 2 + vsize * 2] = -z2;
 		}
 	}
 
 	void mirror_axis_y(float* data, int len) {
 		float x1, x2, x3, y1, y2, y3, z1, z2, z3;
+		int vsize = meshops::vertex_size;
+		int iteration = vsize * 3;
 
-		for (int i = 0; i < len; i += meshops::vertex_size * 3) {
+		for (int i = 0; i < len; i += iteration) {
 			x1 = data[i + 0];
 			y1 = data[i + 1];
 			z1 = data[i + 2];
-			x2 = data[i + 0 + meshops::vertex_size];
-			y2 = data[i + 1 + meshops::vertex_size];
-			z2 = data[i + 2 + meshops::vertex_size];
-			x3 = data[i + 0 + meshops::vertex_size * 2];
-			y3 = data[i + 1 + meshops::vertex_size * 2];
-			z3 = data[i + 2 + meshops::vertex_size * 2];
+			x2 = data[i + 0 + vsize];
+			y2 = data[i + 1 + vsize];
+			z2 = data[i + 2 + vsize];
+			x3 = data[i + 0 + vsize * 2];
+			y3 = data[i + 1 + vsize * 2];
+			z3 = data[i + 2 + vsize * 2];
 			data[i + 0] = -x1;
 			data[i + 1] = -y1;
 			data[i + 2] = -z1;
-			data[i + 0 + meshops::vertex_size] = -x3;
-			data[i + 1 + meshops::vertex_size] = -y3;
-			data[i + 2 + meshops::vertex_size] = -z3;
-			data[i + 0 + meshops::vertex_size * 2] = -x2;
-			data[i + 1 + meshops::vertex_size * 2] = -y2;
-			data[i + 2 + meshops::vertex_size * 2] = -z2;
+			data[i + 0 + vsize] = -x3;
+			data[i + 1 + vsize] = -y3;
+			data[i + 2 + vsize] = -z3;
+			data[i + 0 + vsize * 2] = -x2;
+			data[i + 1 + vsize * 2] = -y2;
+			data[i + 2 + vsize * 2] = -z2;
 		}
 	}
 
 	void mirror_axis_z(float* data, int len) {
 		float x1, x2, x3, y1, y2, y3, z1, z2, z3;
+		int vsize = meshops::vertex_size;
+		int iteration = vsize * 3;
 
-		for (int i = 0; i < len; i += meshops::vertex_size * 3) {
+		for (int i = 0; i < len; i += iteration) {
 			x1 = data[i + 0];
 			y1 = data[i + 1];
 			z1 = data[i + 2];
-			x2 = data[i + 0 + meshops::vertex_size];
-			y2 = data[i + 1 + meshops::vertex_size];
-			z2 = data[i + 2 + meshops::vertex_size];
-			x3 = data[i + 0 + meshops::vertex_size * 2];
-			y3 = data[i + 1 + meshops::vertex_size * 2];
-			z3 = data[i + 2 + meshops::vertex_size * 2];
+			x2 = data[i + 0 + vsize];
+			y2 = data[i + 1 + vsize];
+			z2 = data[i + 2 + vsize];
+			x3 = data[i + 0 + vsize * 2];
+			y3 = data[i + 1 + vsize * 2];
+			z3 = data[i + 2 + vsize * 2];
 			data[i + 0] = -x1;
 			data[i + 1] = -y1;
 			data[i + 2] = -z1;
-			data[i + 0 + meshops::vertex_size] = -x3;
-			data[i + 1 + meshops::vertex_size] = -y3;
-			data[i + 2 + meshops::vertex_size] = -z3;
-			data[i + 0 + meshops::vertex_size * 2] = -x2;
-			data[i + 1 + meshops::vertex_size * 2] = -y2;
-			data[i + 2 + meshops::vertex_size * 2] = -z2;
+			data[i + 0 + vsize] = -x3;
+			data[i + 1 + vsize] = -y3;
+			data[i + 2 + vsize] = -z3;
+			data[i + 0 + vsize * 2] = -x2;
+			data[i + 1 + vsize * 2] = -y2;
+			data[i + 2 + vsize * 2] = -z2;
 		}
 	}
 
 	// texture
 	void flip_tex_u(float* data, int len) {
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		int vsize = meshops::vertex_size;
+		for (int i = 0; i < len; i += vsize) {
 			data[i + 6] = 1 - data[i + 6];
 		}
 	}
 
 	void flip_tex_v(float* data, int len) {
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		int vsize = meshops::vertex_size;
+		for (int i = 0; i < len; i += vsize) {
 			data[i + 7] = 1 - data[i + 7];
 		}
 	}
 
 	// colour
 	void set_colour(float* data, int len, int colour) {
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		int vsize = meshops::vertex_size;
+		for (int i = 0; i < len; i += vsize) {
 			((unsigned int*)data)[i + 8] |= colour;
 		}
 	}
 
 	void set_alpha(float* data, int len, int alpha) {
+		int vsize = meshops::vertex_size;
 		unsigned int a = alpha << 24;
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		for (int i = 0; i < len; i += vsize) {
 			((unsigned int*)data)[i + 8] |= a;
 		}
 	}
 
 	void set_colour_and_alpha(float* data, int len, int colour, int alpha) {
+		int vsize = meshops::vertex_size;
 		unsigned int value = colour | (alpha << 24);
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		for (int i = 0; i < len; i += vsize) {
 			((int*)data)[i + 8] = value;
 		}
 	}
 
 	void invert_alpha(float* data, int len) {
-		for (int i = 0; i < len; i += meshops::vertex_size) {
+		int vsize = meshops::vertex_size;
+		for (int i = 0; i < len; i += vsize) {
 			unsigned int value = ((unsigned int*)data)[i + 8];
 			unsigned int a = (0xff - (value >> 24)) << 24;
 			((unsigned int*)data)[i + 8] = a | (value & 0x00ffffff);
@@ -245,28 +268,29 @@ namespace meshops {
 		Triangle triangle;
 		Vector3 e1, e2;
 		Vector3 normal;
+		int vsize = meshops::vertex_size;
 
 		for (int i = 0; i < len; i += meshops::vertex_size * 3) {
 			triangle.a.x = data[i + 0];
 			triangle.a.y = data[i + 1];
 			triangle.a.z = data[i + 2];
-			triangle.b.x = data[i + 0 + meshops::vertex_size];
-			triangle.b.y = data[i + 1 + meshops::vertex_size];
-			triangle.b.z = data[i + 2 + meshops::vertex_size];
-			triangle.c.x = data[i + 0 + meshops::vertex_size * 2];
-			triangle.c.y = data[i + 1 + meshops::vertex_size * 2];
-			triangle.c.z = data[i + 2 + meshops::vertex_size * 2];
+			triangle.b.x = data[i + 0 + vsize];
+			triangle.b.y = data[i + 1 + vsize];
+			triangle.b.z = data[i + 2 + vsize];
+			triangle.c.x = data[i + 0 + vsize * 2];
+			triangle.c.y = data[i + 1 + vsize * 2];
+			triangle.c.z = data[i + 2 + vsize * 2];
 
 			if (triangle.a.z == triangle.b.z && triangle.a.z == triangle.c.z) {
 				data[i + 3] = 0;
 				data[i + 4] = 0;
 				data[i + 5] = 1;
-				data[i + 3 + meshops::vertex_size] = 0;
-				data[i + 4 + meshops::vertex_size] = 0;
-				data[i + 5 + meshops::vertex_size] = 0;
-				data[i + 3 + meshops::vertex_size * 2] = 0;
-				data[i + 4 + meshops::vertex_size * 2] = 0;
-				data[i + 5 + meshops::vertex_size * 2] = 1;
+				data[i + 3 + vsize] = 0;
+				data[i + 4 + vsize] = 0;
+				data[i + 5 + vsize] = 0;
+				data[i + 3 + vsize * 2] = 0;
+				data[i + 4 + vsize * 2] = 0;
+				data[i + 5 + vsize * 2] = 1;
 			} else {
 				e1.x = triangle.b.x - triangle.a.x;
 				e1.y = triangle.b.y - triangle.a.y;
@@ -284,12 +308,12 @@ namespace meshops {
 				data[i + 3] = normal.x;
 				data[i + 4] = normal.y;
 				data[i + 5] = normal.z;
-				data[i + 3 + meshops::vertex_size] = normal.x;
-				data[i + 4 + meshops::vertex_size] = normal.y;
-				data[i + 5 + meshops::vertex_size] = normal.z;
-				data[i + 3 + meshops::vertex_size * 2] = normal.x;
-				data[i + 4 + meshops::vertex_size * 2] = normal.y;
-				data[i + 5 + meshops::vertex_size * 2] = normal.z;
+				data[i + 3 + vsize] = normal.x;
+				data[i + 4 + vsize] = normal.y;
+				data[i + 5 + vsize] = normal.z;
+				data[i + 3 + vsize * 2] = normal.x;
+				data[i + 4 + vsize * 2] = normal.y;
+				data[i + 5 + vsize * 2] = normal.z;
 			}
 		}
 	}
@@ -299,6 +323,7 @@ namespace meshops {
 		Vector3 vertex, normal, cached;
 		Vector3 cross, e1, e2;
 		float cpl;
+		int vsize = meshops::vertex_size;
 
 		std::map<std::string, Vector3> cache;
 
@@ -306,12 +331,12 @@ namespace meshops {
 			triangle.a.x = data[i + 0];
 			triangle.a.y = data[i + 1];
 			triangle.a.z = data[i + 2];
-			triangle.b.x = data[i + 0 + meshops::vertex_size];
-			triangle.b.y = data[i + 1 + meshops::vertex_size];
-			triangle.b.z = data[i + 2 + meshops::vertex_size];
-			triangle.c.x = data[i + 0 + meshops::vertex_size * 2];
-			triangle.c.y = data[i + 1 + meshops::vertex_size * 2];
-			triangle.c.z = data[i + 2 + meshops::vertex_size * 2];
+			triangle.b.x = data[i + 0 + vsize];
+			triangle.b.y = data[i + 1 + vsize];
+			triangle.b.z = data[i + 2 + vsize];
+			triangle.c.x = data[i + 0 + vsize * 2];
+			triangle.c.y = data[i + 1 + vsize * 2];
+			triangle.c.z = data[i + 2 + vsize * 2];
 
 			e1.x = triangle.b.x - triangle.a.x;
 			e1.y = triangle.b.y - triangle.a.y;
@@ -329,12 +354,12 @@ namespace meshops {
 			data[i + 3] = normal.x;
 			data[i + 4] = normal.y;
 			data[i + 5] = normal.z;
-			data[i + 3 + meshops::vertex_size] = normal.x;
-			data[i + 4 + meshops::vertex_size] = normal.y;
-			data[i + 5 + meshops::vertex_size] = normal.z;
-			data[i + 3 + meshops::vertex_size * 2] = normal.x;
-			data[i + 4 + meshops::vertex_size * 2] = normal.y;
-			data[i + 5 + meshops::vertex_size * 2] = normal.z;
+			data[i + 3 + vsize] = normal.x;
+			data[i + 4 + vsize] = normal.y;
+			data[i + 5 + vsize] = normal.z;
+			data[i + 3 + vsize * 2] = normal.x;
+			data[i + 4 + vsize * 2] = normal.y;
+			data[i + 5 + vsize * 2] = normal.z;
 			
 			std::string keys[] = {
 				std::format("{},{},{}", triangle.a.x, triangle.a.y, triangle.a.z),
