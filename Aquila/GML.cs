ade6f42c31f10ab1eecb6cc7e82dcf19706699d4
Michael <mpeng@optimum.net>
2022-01-20 19:50:52 -0500

    split the gml stuff off of the aquila stuff

---
+++ Aquila/GML.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aquila {
+    public class GML {
+        [DllExport]
+        public static string Version() {
+            return "0.0.0";
+        }
+        [DllExport]
+        public static double CreateGraph() {
+            return 1;
+        }
+        [DllExport]
+        public static double DestroyGraph() {
+            GC.Collect();
+            return 1;
+        }
+        [DllExport]
+        public static double AddNode(double graph, double data) {
+            return 1;
+        }
+        [DllExport]
+        public static double RemoveNode(double graph, double data) {
+            GC.Collect();
+            return 1;
+        }
+        [DllExport]
+        public static double ConnectNode(double graph, double a, double b) {
+            return 1;
+        }
+        [DllExport]
+        public static double DisconnectNodes(double graph, double a, double b) {
+            return 1;
+        }
+        [DllExport]
+        public static double ClearNodes(double graph) {
+            return 1;
+        }
+        [DllExport]
+        public static double Size(double graph) {
+            return 1;
+        }
+        [DllExport]
+        public static double Navigate(double source, double destination) {
+            return 1;
+        }
+    }
+}
