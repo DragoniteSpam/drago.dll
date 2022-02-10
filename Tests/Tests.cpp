091a3a6c2475447f11e928dbed22c35a68b7b283
Michael <mpeng@optimum.net>
2022-02-10 11:12:20 -0500

    updated tests

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -5,23 +5,49 @@
 using namespace std;
 
 int main() {
-	unsigned int data[16] = {
-		0x00000000, 0x00000000, 0x00000000, 0x00000000,
-		0x40404040, 0x40404040, 0x40404040, 0x40404040,
-		0x80808080, 0x80808080, 0x80808080, 0x80808080,
-		0xb0b0b0b0, 0xb0b0b0b0, 0xb0b0b0b0, 0xb0b0b0b0
+	unsigned int data[25] = {
+		0, 10, 20, 50, 100,
+		0, 100, 200, 255, 255,
+		0, 100, 200, 255, 255,
+		0, 100, 200, 255, 255,
 	};
+	/*
+	cout << spriteops::sample(data, 5, 5, 0.0f, 0.0f) << endl;
+	cout << spriteops::sample(data, 5, 5, 0.10f, 0.10f) << endl;
+	cout << spriteops::sample(data, 5, 5, 0.15f, 0.15f) << endl;
+	cout << spriteops::sample(data, 5, 5, 0.20f, 0.60f) << endl;
+	cout << spriteops::sample(data, 5, 5, 0.25f, 0.25f) << endl;
+	cout << spriteops::sample(data, 5, 5, 0.30f, 0.30f) << endl;
+	cout << spriteops::sample(data, 5, 5, 0.35f, 0.35f) << endl;
+	cout << spriteops::sample(data, 5, 5, 0.40f, 0.40f) << endl;
+	cout << spriteops::sample(data, 5, 5, 0.45f, 0.45f) << endl;
+	cout << spriteops::sample(data, 5, 5, 0.5f, 0.5f) << endl;
+	*/
 
-	cout << (void*)spriteops::sample(data, 4, 4, 0.0f, 0.0f) << endl;
-	cout << (void*)spriteops::sample(data, 4, 4, 0.10f, 0.10f) << endl;
-	cout << (void*)spriteops::sample(data, 4, 4, 0.15f, 0.15f) << endl;
-	cout << (void*)spriteops::sample(data, 4, 4, 0.20f, 0.20f) << endl;
-	cout << (void*)spriteops::sample(data, 4, 4, 0.25f, 0.25f) << endl;
-	cout << (void*)spriteops::sample(data, 4, 4, 0.30f, 0.30f) << endl;
-	cout << (void*)spriteops::sample(data, 4, 4, 0.35f, 0.35f) << endl;
-	cout << (void*)spriteops::sample(data, 4, 4, 0.40f, 0.40f) << endl;
-	cout << (void*)spriteops::sample(data, 4, 4, 0.45f, 0.45f) << endl;
-	cout << (void*)spriteops::sample(data, 4, 4, 0.5f, 0.5f) << endl;
+	unsigned int a = 98;
+	unsigned int b = 99;
+	unsigned int c = 94;
+	unsigned int d = 95;
+
+	float f = 0.0;
+	unsigned int f1 = spriteops::merge(a, b, f);
+	unsigned int f2 = spriteops::merge(c, d, f);
+	cout << f1 << ", " << f2 << ": " << spriteops::merge(f1, f2, f) << endl;
+	
+	a = 61;
+	b = 61;
+	c = 59;
+	d = 59;
+
+	f = 0.5;
+	f1 = LERP(a, b, f);
+	f2 = LERP(c, d, f);
+	cout << f1 << ", " << f2 << ": " << LERP(f1, f2, f) << endl;
+
+	f = 1.0;
+	f1 = spriteops::merge(a, b, f);
+	f2 = spriteops::merge(c, d, f);
+	cout << f1 << ", " << f2 << ": " << spriteops::merge(f1, f2, f) << endl;
 
 	///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
 	//btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
