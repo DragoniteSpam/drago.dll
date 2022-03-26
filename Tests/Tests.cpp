5c92950a7ef53176f069f5ca8777ec1df1593ea5
Michael <mpeng@optimum.net>
2022-03-26 13:09:18 -0400

    matrix tests

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -69,28 +69,42 @@ void sprite_tests() {
 	cout << f1 << ", " << f2 << ": " << spriteops::merge(f1, f2, f) << endl;
 }
 
-void matrix_tests() {
-	Matrix4x4 transform = Matrix4x4::Transform(10, 10, 10, 0, 0, 0, 1, 2, 1);
+void matrix_building_tests(Matrix4x4 printme) {
+	for (int i = 0; i < 16; i++) {
+		cout << printme.asArray[i] << " ";
+		if (i % 4 == 3)
+			cout << "\n";
+	}
+}
+
+void matrix_transform_tests() {
+	Matrix4x4 transform = Matrix4x4::Transform(10, 10, 10, 15, 20, 30, 1, 2, 1);
 	Vector4 point = Vector4(5, 5, 0, 1);
 
 	point = transform * point;
 
 	cout << "Transform matrix:\n";
 
-	for (int i = 0; i < 16; i++) {
-		cout << transform.asArray[i] << " ";
-	}
+	matrix_building_tests(transform);
 
 	cout << "\nFinal position:\n";
 
 	cout << point.x << ", " << point.y << ", " << point.z;
 
 	cout << endl;
-
-	
 }
 
 int main() {
-	matrix_tests();
+	Matrix4x4 translate = Matrix4x4::Transform(10, 10, 10, 0, 0, 0, 1, 1, 1);
+	Matrix4x4 rotate = Matrix4x4::Transform(0, 0, 0, 15, 20, 30, 1, 1, 1);
+	Matrix4x4 scale = Matrix4x4::Transform(0, 0, 0, 0, 0, 0, 1, 2, 1);
+
+	cout << "rotate scale translate" << endl;
+
+	matrix_building_tests((rotate * scale) * translate);
+
+	cout << "scale rotate translate" << endl;
+
+	matrix_building_tests((scale * rotate) * translate);
 	return 0;
 }
