18c9da418dabf2d15931be7f162bfb0fd38fd802
Michael <mpeng@optimum.net>
2022-10-24 02:44:38 -0400

    basic unity converter model

--- GUT/GUT.cs
+++ GUT/GUT.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace GUT
-{
-    public class GUT
-    {
+namespace GUT {
+    public class GUT {
+        public void Reset() {
+
+        }
+
+        public void Open() {
+            using (OpenFileDialog finder = new OpenFileDialog()) {
+                finder.Filter = "Unity scenes (*.unity)|*.unity";
+
+                if (finder.ShowDialog() == DialogResult.OK) {
+                    Reset();
+                }
+            }
+        }
+
+        public void Export() {
+            using (SaveFileDialog finder = new SaveFileDialog()) {
+                finder.Filter = /* come up with a file extension and format later */"";
+
+                if (finder.ShowDialog() == DialogResult.OK) {
+
+                }
+            }
+        }
     }
 }
