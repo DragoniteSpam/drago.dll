584244bdc6aa0e931a181a25f02b6845e9435fc8
Michael <mpeng@optimum.net>
2023-07-06 14:06:32 -0400

    moved assimp stuff to its own project

--- AssimpOperations/assops.cpp
+++ AssimpOperations/assops.cpp
@@ -6,7 +6,7 @@ float assops::convert_fbx(const std::string filename, std::string output_filenam
     // Create an instance of the Importer class
     Assimp::Importer importer;
     Assimp::Exporter exporter;
-
+    
     // And have it read the given file with some example postprocessing
     // Usually - if speed is not the most important aspect for you - you'll
     // probably to request more postprocessing than we do in this example.
@@ -27,6 +27,6 @@ float assops::convert_fbx(const std::string filename, std::string output_filenam
 
     exporter.Export(scene, "obj", output_filename);
     std::cout << "exported the converted file" << std::endl;
-
+    
     return 1.0;
 }
