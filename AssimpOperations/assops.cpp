1bf0b0b5b908b039ca1310f67b18537a18dfc497
Michael <mpeng@optimum.net>
2023-07-10 12:17:34 -0400

    assops 0.1

--- AssimpOperations/assops.cpp
+++ AssimpOperations/assops.cpp
@@ -1,11 +1,9 @@
 #include "assops.h"
 
-#include <iostream>
-
 namespace assops {
     Assimp::Importer importer;
     Assimp::Exporter exporter;
-    std::vector<std::string> conversion_files;
+    //std::vector<std::string> conversion_files;
 
     const unsigned int import_flags = aiProcess_CalcTangentSpace |
         aiProcess_Triangulate |
@@ -17,24 +15,25 @@ namespace assops {
     const char* version() {
         return __DRAGO_ASS_OP;
     }
-
+    /*
     float add_file(const char* input_file, const char* output_file) {
         //convert_obj(input_file, output_file);
         conversion_files.push_back(std::string(input_file));
         conversion_files.push_back(std::string(output_file));
     }
-
+    */
     float convert_x(const std::string filename, const std::string output_filename, const std::string output_type) {
         const aiScene* scene = assops::importer.ReadFile(filename, import_flags);
 
-        if (scene == nullptr)
+        if (scene == nullptr) {
             return 0.0;
+        }
 
         assops::exporter.Export(scene, output_type, output_filename);
 
         return 1.0;
     }
-    
+    /*
     void convert_all_x(const std::string output_type) {
         for (int i = 0; i < conversion_files.size(); i += 2) {
             std::string source = conversion_files[i];
@@ -43,21 +42,25 @@ namespace assops {
         }
         conversion_files.clear();
     }
-
+    */
     float convert_obj(const std::string filename, const std::string output_filename) {
         return convert_x(filename, output_filename, "obj");
     }
-
+    /*
     void convert_all_obj() {
         convert_all_x("obj");
     }
-
+    */
     float convert_fbx(const std::string filename, const std::string output_filename) {
         return convert_x(filename, output_filename, "fbx");
     }
 
     float convert_dae(const std::string filename, const std::string output_filename) {
         return convert_x(filename, output_filename, "dae");
+    }
+
+    float convert_3ds(const std::string filename, const std::string output_filename) {
+        return convert_x(filename, output_filename, "3ds");
     }
 
     float convert_stl(const std::string filename, const std::string output_filename) {
@@ -79,13 +82,17 @@ namespace assops {
     float convert_plyb(const std::string filename, const std::string output_filename) {
         return convert_x(filename, output_filename, "plyb");
     }
-
+    /*
     void convert_all_fbx() {
         convert_all_x("fbx");
     }
 
     void convert_all_dae() {
         convert_all_x("dae");
+    }
+
+    void convert_all_3ds() {
+        convert_all_x("3ds");
     }
 
     void convert_all_stl() {
@@ -107,4 +114,5 @@ namespace assops {
     void convert_all_glb() {
         convert_all_x("glb");
     }
+    */
 }
--- AssimpOperations/assops.h
+++ AssimpOperations/assops.h
@@ -1,44 +1,48 @@
 #ifndef __DRAGO_ASS_OP
-#define __DRAGO_ASS_OP "0.0"
+#define __DRAGO_ASS_OP "0.1"
 
 #include <assimp/Importer.hpp>
 #include <assimp/Exporter.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
-#include <vector>
+//#include <vector>
 
 namespace assops {
     extern Assimp::Importer importer;
     extern Assimp::Exporter exporter;
-    extern std::vector<std::string> conversion_files;
+    //extern std::vector<std::string> conversion_files;
 
     const char* version();
     
-    float add_file(const char*, const char*);
+    //float add_file(const char*, const char*);
 
     float convert_x(const std::string, const std::string, const std::string);
-    void convert_all_x(const std::string);
+    //void convert_all_x(const std::string);
 
     // used for importing
     float convert_obj(const std::string, std::string);
-    void convert_all_obj();
+    //void convert_all_obj();
 
     // used for exporting
     float convert_fbx(const std::string, const std::string);
     float convert_dae(const std::string, const std::string);
+    float convert_3ds(const std::string, const std::string);
     float convert_stl(const std::string, const std::string);
     float convert_gltf(const std::string, const std::string);
     float convert_glb(const std::string, const std::string);
     float convert_ply(const std::string, const std::string);
     float convert_plyb(const std::string, const std::string);
+    /*
     void convert_all_fbx();
     void convert_all_dae();
+    void convert_all_3ds();
     void convert_all_stl();
     void convert_all_ply();
     void convert_all_plyb();
     void convert_all_gltf();
     void convert_all_glb();
+    */
 }
 
 #endif
