d8e4e64800de1d0f4dc13f8c4db929879fb0ad5d
Michael <mpeng@optimum.net>
2024-12-01 18:22:30 -0500

    shape checkers are now all const

--- ThreeDCollisions/Point.cpp
+++ ThreeDCollisions/Point.cpp
@@ -8,37 +8,37 @@ public:
 		this->position->Set(position);
 	}
 
-	bool CheckPoint(const ShapePoint* point) override {
+	bool CheckPoint(const ShapePoint* point) const override {
 		return this->position->Equals(point->position);
 	}
-	bool CheckSphere(const ShapeSphere* point) override {
+	bool CheckSphere(const ShapeSphere* point) const override {
 		return true;
 	}
-	bool CheckAABB(const ShapeAABB* point) override {
+	bool CheckAABB(const ShapeAABB* point) const override {
 		return true;
 	}
-	bool CheckOBB(const ShapeOBB* point) override {
+	bool CheckOBB(const ShapeOBB* point) const override {
 		return true;
 	}
-	bool CheckPlane(const ShapePlane* point) override {
+	bool CheckPlane(const ShapePlane* point) const override {
 		return true;
 	}
-	bool CheckCapsule(const ShapeCapsule* point) override {
+	bool CheckCapsule(const ShapeCapsule* point) const override {
 		return true;
 	}
-	bool CheckTriangle(const ShapeTriangle* point) override {
+	bool CheckTriangle(const ShapeTriangle* point) const override {
 		return true;
 	}
-	bool CheckMesh(const ShapeMesh* point) override {
+	bool CheckMesh(const ShapeMesh* point) const override {
 		return true;
 	}
-	bool CheckModel(const ShapeModel* point) override {
+	bool CheckModel(const ShapeModel* point) const override {
 		return true;
 	}
-	bool CheckLine(const ShapeLine* point) override {
+	bool CheckLine(const ShapeLine* point) const override {
 		return true;
 	}
-	bool CheckRay(const ShapeRay* point) override {
+	bool CheckRay(const ShapeRay* point) const override {
 		return true;
 	}
 };
--- ThreeDCollisions/Shapes.h
+++ ThreeDCollisions/Shapes.h
@@ -4,180 +4,180 @@
 
 class Shape {
 public:
-	virtual bool CheckPoint(const ShapePoint* point);
-	virtual bool CheckSphere(const ShapeSphere* point);
-	virtual bool CheckAABB(const ShapeAABB* point);
-	virtual bool CheckOBB(const ShapeOBB* point);
-	virtual bool CheckPlane(const ShapePlane* point);
-	virtual bool CheckCapsule(const ShapeCapsule* point);
-	virtual bool CheckTriangle(const ShapeTriangle* point);
-	virtual bool CheckMesh(const ShapeMesh* point);
-	virtual bool CheckModel(const ShapeModel* point);
-	virtual bool CheckLine(const ShapeLine* point);
-	virtual bool CheckRay(const ShapeRay* point);
+	virtual bool CheckPoint(const ShapePoint* point) const { return true; }
+	virtual bool CheckSphere(const ShapeSphere* point) const { return true; }
+	virtual bool CheckAABB(const ShapeAABB* point) const { return true; }
+	virtual bool CheckOBB(const ShapeOBB* point) const { return true; }
+	virtual bool CheckPlane(const ShapePlane* point) const { return true; }
+	virtual bool CheckCapsule(const ShapeCapsule* point) const { return true; }
+	virtual bool CheckTriangle(const ShapeTriangle* point) const { return true; }
+	virtual bool CheckMesh(const ShapeMesh* point) const { return true; }
+	virtual bool CheckModel(const ShapeModel* point) const { return true; }
+	virtual bool CheckLine(const ShapeLine* point) const { return true; }
+	virtual bool CheckRay(const ShapeRay* point) const { return true; }
 };
 
 class ShapePoint: Shape {
 public:
-	bool CheckPoint(const ShapePoint* point) override;
-	bool CheckSphere(const ShapeSphere* point) override;
-	bool CheckAABB(const ShapeAABB* point) override;
-	bool CheckOBB(const ShapeOBB* point) override;
-	bool CheckPlane(const ShapePlane* point) override;
-	bool CheckCapsule(const ShapeCapsule* point) override;
-	bool CheckTriangle(const ShapeTriangle* point) override;
-	bool CheckMesh(const ShapeMesh* point) override;
-	bool CheckModel(const ShapeModel* point) override;
-	bool CheckLine(const ShapeLine* point) override;
-	bool CheckRay(const ShapeRay* point) override;
+	bool CheckPoint(const ShapePoint* point) const override;
+	bool CheckSphere(const ShapeSphere* point) const override;
+	bool CheckAABB(const ShapeAABB* point) const override;
+	bool CheckOBB(const ShapeOBB* point) const override;
+	bool CheckPlane(const ShapePlane* point) const override;
+	bool CheckCapsule(const ShapeCapsule* point) const override;
+	bool CheckTriangle(const ShapeTriangle* point) const override;
+	bool CheckMesh(const ShapeMesh* point) const override;
+	bool CheckModel(const ShapeModel* point) const override;
+	bool CheckLine(const ShapeLine* point) const override;
+	bool CheckRay(const ShapeRay* point) const override;
 };
 
 class ShapeSphere: Shape {
 public:
-	bool CheckPoint(const ShapePoint* point) override;
-	bool CheckSphere(const ShapeSphere* point) override;
-	bool CheckAABB(const ShapeAABB* point) override;
-	bool CheckOBB(const ShapeOBB* point) override;
-	bool CheckPlane(const ShapePlane* point) override;
-	bool CheckCapsule(const ShapeCapsule* point) override;
-	bool CheckTriangle(const ShapeTriangle* point) override;
-	bool CheckMesh(const ShapeMesh* point) override;
-	bool CheckModel(const ShapeModel* point) override;
-	bool CheckLine(const ShapeLine* point) override;
-	bool CheckRay(const ShapeRay* point) override;
+	bool CheckPoint(const ShapePoint* point) const override;
+	bool CheckSphere(const ShapeSphere* point) const override;
+	bool CheckAABB(const ShapeAABB* point) const override;
+	bool CheckOBB(const ShapeOBB* point) const override;
+	bool CheckPlane(const ShapePlane* point) const override;
+	bool CheckCapsule(const ShapeCapsule* point) const override;
+	bool CheckTriangle(const ShapeTriangle* point) const override;
+	bool CheckMesh(const ShapeMesh* point) const override;
+	bool CheckModel(const ShapeModel* point) const override;
+	bool CheckLine(const ShapeLine* point) const override;
+	bool CheckRay(const ShapeRay* point) const override;
 };
 
 class ShapeAABB: Shape {
 public:
-	bool CheckPoint(const ShapePoint* point) override;
-	bool CheckSphere(const ShapeSphere* point) override;
-	bool CheckAABB(const ShapeAABB* point) override;
-	bool CheckOBB(const ShapeOBB* point) override;
-	bool CheckPlane(const ShapePlane* point) override;
-	bool CheckCapsule(const ShapeCapsule* point) override;
-	bool CheckTriangle(const ShapeTriangle* point) override;
-	bool CheckMesh(const ShapeMesh* point) override;
-	bool CheckModel(const ShapeModel* point) override;
-	bool CheckLine(const ShapeLine* point) override;
-	bool CheckRay(const ShapeRay* point) override;
+	bool CheckPoint(const ShapePoint* point) const override;
+	bool CheckSphere(const ShapeSphere* point) const override;
+	bool CheckAABB(const ShapeAABB* point) const override;
+	bool CheckOBB(const ShapeOBB* point) const override;
+	bool CheckPlane(const ShapePlane* point) const override;
+	bool CheckCapsule(const ShapeCapsule* point) const override;
+	bool CheckTriangle(const ShapeTriangle* point) const override;
+	bool CheckMesh(const ShapeMesh* point) const override;
+	bool CheckModel(const ShapeModel* point) const override;
+	bool CheckLine(const ShapeLine* point) const override;
+	bool CheckRay(const ShapeRay* point) const override;
 };
 
 class ShapeOBB: Shape {
 public:
-	bool CheckPoint(const ShapePoint* point) override;
-	bool CheckSphere(const ShapeSphere* point) override;
-	bool CheckAABB(const ShapeAABB* point) override;
-	bool CheckOBB(const ShapeOBB* point) override;
-	bool CheckPlane(const ShapePlane* point) override;
-	bool CheckCapsule(const ShapeCapsule* point) override;
-	bool CheckTriangle(const ShapeTriangle* point) override;
-	bool CheckMesh(const ShapeMesh* point) override;
-	bool CheckModel(const ShapeModel* point) override;
-	bool CheckLine(const ShapeLine* point) override;
-	bool CheckRay(const ShapeRay* point) override;
+	bool CheckPoint(const ShapePoint* point) const override;
+	bool CheckSphere(const ShapeSphere* point) const override;
+	bool CheckAABB(const ShapeAABB* point) const override;
+	bool CheckOBB(const ShapeOBB* point) const override;
+	bool CheckPlane(const ShapePlane* point) const override;
+	bool CheckCapsule(const ShapeCapsule* point) const override;
+	bool CheckTriangle(const ShapeTriangle* point) const override;
+	bool CheckMesh(const ShapeMesh* point) const override;
+	bool CheckModel(const ShapeModel* point) const override;
+	bool CheckLine(const ShapeLine* point) const override;
+	bool CheckRay(const ShapeRay* point) const override;
 };
 
 class ShapePlane: Shape {
 public:
-	bool CheckPoint(const ShapePoint* point) override;
-	bool CheckSphere(const ShapeSphere* point) override;
-	bool CheckAABB(const ShapeAABB* point) override;
-	bool CheckOBB(const ShapeOBB* point) override;
-	bool CheckPlane(const ShapePlane* point) override;
-	bool CheckCapsule(const ShapeCapsule* point) override;
-	bool CheckTriangle(const ShapeTriangle* point) override;
-	bool CheckMesh(const ShapeMesh* point) override;
-	bool CheckModel(const ShapeModel* point) override;
-	bool CheckLine(const ShapeLine* point) override;
-	bool CheckRay(const ShapeRay* point) override;
+	bool CheckPoint(const ShapePoint* point) const override;
+	bool CheckSphere(const ShapeSphere* point) const override;
+	bool CheckAABB(const ShapeAABB* point) const override;
+	bool CheckOBB(const ShapeOBB* point) const override;
+	bool CheckPlane(const ShapePlane* point) const override;
+	bool CheckCapsule(const ShapeCapsule* point) const override;
+	bool CheckTriangle(const ShapeTriangle* point) const override;
+	bool CheckMesh(const ShapeMesh* point) const override;
+	bool CheckModel(const ShapeModel* point) const override;
+	bool CheckLine(const ShapeLine* point) const override;
+	bool CheckRay(const ShapeRay* point) const override;
 };
 
 class ShapeCapsule: Shape {
 public:
-	bool CheckPoint(const ShapePoint* point) override;
-	bool CheckSphere(const ShapeSphere* point) override;
-	bool CheckAABB(const ShapeAABB* point) override;
-	bool CheckOBB(const ShapeOBB* point) override;
-	bool CheckPlane(const ShapePlane* point) override;
-	bool CheckCapsule(const ShapeCapsule* point) override;
-	bool CheckTriangle(const ShapeTriangle* point) override;
-	bool CheckMesh(const ShapeMesh* point) override;
-	bool CheckModel(const ShapeModel* point) override;
-	bool CheckLine(const ShapeLine* point) override;
-	bool CheckRay(const ShapeRay* point) override;
+	bool CheckPoint(const ShapePoint* point) const override;
+	bool CheckSphere(const ShapeSphere* point) const override;
+	bool CheckAABB(const ShapeAABB* point) const override;
+	bool CheckOBB(const ShapeOBB* point) const override;
+	bool CheckPlane(const ShapePlane* point) const override;
+	bool CheckCapsule(const ShapeCapsule* point) const override;
+	bool CheckTriangle(const ShapeTriangle* point) const override;
+	bool CheckMesh(const ShapeMesh* point) const override;
+	bool CheckModel(const ShapeModel* point) const override;
+	bool CheckLine(const ShapeLine* point) const override;
+	bool CheckRay(const ShapeRay* point) const override;
 };
 
 class ShapeTriangle: Shape {
 public:
-	bool CheckPoint(const ShapePoint* point) override;
-	bool CheckSphere(const ShapeSphere* point) override;
-	bool CheckAABB(const ShapeAABB* point) override;
-	bool CheckOBB(const ShapeOBB* point) override;
-	bool CheckPlane(const ShapePlane* point) override;
-	bool CheckCapsule(const ShapeCapsule* point) override;
-	bool CheckTriangle(const ShapeTriangle* point) override;
-	bool CheckMesh(const ShapeMesh* point) override;
-	bool CheckModel(const ShapeModel* point) override;
-	bool CheckLine(const ShapeLine* point) override;
-	bool CheckRay(const ShapeRay* point) override;
+	bool CheckPoint(const ShapePoint* point) const override;
+	bool CheckSphere(const ShapeSphere* point) const override;
+	bool CheckAABB(const ShapeAABB* point) const override;
+	bool CheckOBB(const ShapeOBB* point) const override;
+	bool CheckPlane(const ShapePlane* point) const override;
+	bool CheckCapsule(const ShapeCapsule* point) const override;
+	bool CheckTriangle(const ShapeTriangle* point) const override;
+	bool CheckMesh(const ShapeMesh* point) const override;
+	bool CheckModel(const ShapeModel* point) const override;
+	bool CheckLine(const ShapeLine* point) const override;
+	bool CheckRay(const ShapeRay* point) const override;
 };
 
 class ShapeMesh: Shape {
 public:
-	bool CheckPoint(const ShapePoint* point) override;
-	bool CheckSphere(const ShapeSphere* point) override;
-	bool CheckAABB(const ShapeAABB* point) override;
-	bool CheckOBB(const ShapeOBB* point) override;
-	bool CheckPlane(const ShapePlane* point) override;
-	bool CheckCapsule(const ShapeCapsule* point) override;
-	bool CheckTriangle(const ShapeTriangle* point) override;
-	bool CheckMesh(const ShapeMesh* point) override;
-	bool CheckModel(const ShapeModel* point) override;
-	bool CheckLine(const ShapeLine* point) override;
-	bool CheckRay(const ShapeRay* point) override;
+	bool CheckPoint(const ShapePoint* point) const override;
+	bool CheckSphere(const ShapeSphere* point) const override;
+	bool CheckAABB(const ShapeAABB* point) const override;
+	bool CheckOBB(const ShapeOBB* point) const override;
+	bool CheckPlane(const ShapePlane* point) const override;
+	bool CheckCapsule(const ShapeCapsule* point) const override;
+	bool CheckTriangle(const ShapeTriangle* point) const override;
+	bool CheckMesh(const ShapeMesh* point) const override;
+	bool CheckModel(const ShapeModel* point) const override;
+	bool CheckLine(const ShapeLine* point) const override;
+	bool CheckRay(const ShapeRay* point) const override;
 };
 
 class ShapeModel: Shape {
 public:
-	bool CheckPoint(const ShapePoint* point) override;
-	bool CheckSphere(const ShapeSphere* point) override;
-	bool CheckAABB(const ShapeAABB* point) override;
-	bool CheckOBB(const ShapeOBB* point) override;
-	bool CheckPlane(const ShapePlane* point) override;
-	bool CheckCapsule(const ShapeCapsule* point) override;
-	bool CheckTriangle(const ShapeTriangle* point) override;
-	bool CheckMesh(const ShapeMesh* point) override;
-	bool CheckModel(const ShapeModel* point) override;
-	bool CheckLine(const ShapeLine* point) override;
-	bool CheckRay(const ShapeRay* point) override;
+	bool CheckPoint(const ShapePoint* point) const override;
+	bool CheckSphere(const ShapeSphere* point) const override;
+	bool CheckAABB(const ShapeAABB* point) const override;
+	bool CheckOBB(const ShapeOBB* point) const override;
+	bool CheckPlane(const ShapePlane* point) const override;
+	bool CheckCapsule(const ShapeCapsule* point) const override;
+	bool CheckTriangle(const ShapeTriangle* point) const override;
+	bool CheckMesh(const ShapeMesh* point) const override;
+	bool CheckModel(const ShapeModel* point) const override;
+	bool CheckLine(const ShapeLine* point) const override;
+	bool CheckRay(const ShapeRay* point) const override;
 };
 
 class ShapeRay: Shape {
 public:
-	bool CheckPoint(const ShapePoint* point) override;
-	bool CheckSphere(const ShapeSphere* point) override;
-	bool CheckAABB(const ShapeAABB* point) override;
-	bool CheckOBB(const ShapeOBB* point) override;
-	bool CheckPlane(const ShapePlane* point) override;
-	bool CheckCapsule(const ShapeCapsule* point) override;
-	bool CheckTriangle(const ShapeTriangle* point) override;
-	bool CheckMesh(const ShapeMesh* point) override;
-	bool CheckModel(const ShapeModel* point) override;
-	bool CheckLine(const ShapeLine* point) override;
-	bool CheckRay(const ShapeRay* point) override;
+	bool CheckPoint(const ShapePoint* point) const override;
+	bool CheckSphere(const ShapeSphere* point) const override;
+	bool CheckAABB(const ShapeAABB* point) const override;
+	bool CheckOBB(const ShapeOBB* point) const override;
+	bool CheckPlane(const ShapePlane* point) const override;
+	bool CheckCapsule(const ShapeCapsule* point) const override;
+	bool CheckTriangle(const ShapeTriangle* point) const override;
+	bool CheckMesh(const ShapeMesh* point) const override;
+	bool CheckModel(const ShapeModel* point) const override;
+	bool CheckLine(const ShapeLine* point) const override;
+	bool CheckRay(const ShapeRay* point) const override;
 };
 
 class ShapeLine: Shape {
 public:
-	bool CheckPoint(const ShapePoint* point) override;
-	bool CheckSphere(const ShapeSphere* point) override;
-	bool CheckAABB(const ShapeAABB* point) override;
-	bool CheckOBB(const ShapeOBB* point) override;
-	bool CheckPlane(const ShapePlane* point) override;
-	bool CheckCapsule(const ShapeCapsule* point) override;
-	bool CheckTriangle(const ShapeTriangle* point) override;
-	bool CheckMesh(const ShapeMesh* point) override;
-	bool CheckModel(const ShapeModel* point) override;
-	bool CheckLine(const ShapeLine* point) override;
-	bool CheckRay(const ShapeRay* point) override;
+	bool CheckPoint(const ShapePoint* point) const override;
+	bool CheckSphere(const ShapeSphere* point) const override;
+	bool CheckAABB(const ShapeAABB* point) const override;
+	bool CheckOBB(const ShapeOBB* point) const override;
+	bool CheckPlane(const ShapePlane* point) const override;
+	bool CheckCapsule(const ShapeCapsule* point) const override;
+	bool CheckTriangle(const ShapeTriangle* point) const override;
+	bool CheckMesh(const ShapeMesh* point) const override;
+	bool CheckModel(const ShapeModel* point) const override;
+	bool CheckLine(const ShapeLine* point) const override;
+	bool CheckRay(const ShapeRay* point) const override;
 };
