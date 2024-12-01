8462fabdf7b95ece877f495abcfc57d73b9ca291
Michael <mpeng@optimum.net>
2024-12-01 16:38:58 -0500

    ooh there's 128-bit vectors

--- main/dragomath.cpp
+++ main/dragomath.cpp
@@ -69,9 +69,9 @@ Matrix4x4 operator*(const Matrix4x4& a, const Matrix4x4& b) {
 Vector4 operator+(const Vector4& a, const Vector4& b) {
 	//return Vector4{ a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w };
 
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, a.w, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, b.w, 0, 0, 0, 0);
-	__m256 result = _mm256_add_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, a.w);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, b.w);
+	__m128 result = _mm_add_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
@@ -79,9 +79,9 @@ Vector4 operator+(const Vector4& a, const Vector4& b) {
 Vector4 operator-(const Vector4& a, const Vector4& b) {
 	//return Vector4{ a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w };
 
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, a.w, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, b.w, 0, 0, 0, 0);
-	__m256 result = _mm256_sub_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, a.w);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, b.w);
+	__m128 result = _mm_sub_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
@@ -89,9 +89,9 @@ Vector4 operator-(const Vector4& a, const Vector4& b) {
 Vector4 operator*(const Vector4& a, const Vector4& b) {
 	//return Vector4{ a.x * b.x, a.y * b.y, a.z * b.z, a.w * b.w };
 
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, a.w, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, b.w, 0, 0, 0, 0);
-	__m256 result = _mm256_mul_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, a.w);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, b.w);
+	__m128 result = _mm_mul_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
@@ -99,168 +99,168 @@ Vector4 operator*(const Vector4& a, const Vector4& b) {
 Vector4 operator/(const Vector4& a, const Vector4& b) {
 	//return Vector4{ a.x / b.x, a.y / b.y, a.z / b.z, a.w / b.w };
 
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, a.w, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, b.w, 0, 0, 0, 0);
-	__m256 result = _mm256_div_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, a.w);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, b.w);
+	__m128 result = _mm_div_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
 Vector4 operator+(const Vector4& a, float b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, a.w, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b, b, b, b, 0, 0, 0, 0);
-	__m256 result = _mm256_add_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, a.w);
+	__m128 vb = _mm_set_ps(b, b, b, b);
+	__m128 result = _mm_add_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
 
 Vector4 operator-(const Vector4& a, float b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, a.w, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b, b, b, b, 0, 0, 0, 0);
-	__m256 result = _mm256_sub_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, a.w);
+	__m128 vb = _mm_set_ps(b, b, b, b);
+	__m128 result = _mm_sub_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
 
 Vector4 operator*(const Vector4& a, float b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, a.w, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b, b, b, b, 0, 0, 0, 0);
-	__m256 result = _mm256_mul_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, a.w);
+	__m128 vb = _mm_set_ps(b, b, b, b);
+	__m128 result = _mm_mul_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
 
 Vector4 operator/(const Vector4& a, float b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, a.w, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b, b, b, b, 0, 0, 0, 0);
-	__m256 result = _mm256_div_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, a.w);
+	__m128 vb = _mm_set_ps(b, b, b, b);
+	__m128 result = _mm_div_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
 
 Vector4 operator+(float a, const Vector4& b) {
-	__m256 va = _mm256_set_ps(a, a, a, a, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, b.w, 0, 0, 0, 0);
-	__m256 result = _mm256_add_ps(va, vb);
+	__m128 va = _mm_set_ps(a, a, a, a);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, b.w);
+	__m128 result = _mm_add_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
 
 Vector4 operator-(float a, const Vector4& b) {
-	__m256 va = _mm256_set_ps(a, a, a, a, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, b.w, 0, 0, 0, 0);
-	__m256 result = _mm256_sub_ps(va, vb);
+	__m128 va = _mm_set_ps(a, a, a, a);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, b.w);
+	__m128 result = _mm_sub_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
 
 Vector4 operator*(float a, const Vector4& b) {
-	__m256 va = _mm256_set_ps(a, a, a, a, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, b.w, 0, 0, 0, 0);
-	__m256 result = _mm256_mul_ps(va, vb);
+	__m128 va = _mm_set_ps(a, a, a, a);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, b.w);
+	__m128 result = _mm_mul_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
 
 Vector4 operator/(float a, const Vector4& b) {
-	__m256 va = _mm256_set_ps(a, a, a, a, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, b.w, 0, 0, 0, 0);
-	__m256 result = _mm256_div_ps(va, vb);
+	__m128 va = _mm_set_ps(a, a, a, a);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, b.w);
+	__m128 result = _mm_div_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector4{ out[7], out[6], out[5], out[4] };
 }
 
 Vector3 operator+(const Vector3& a, const Vector3& b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_add_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, 0);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, 0);
+	__m128 result = _mm_add_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
 
 Vector3 operator-(const Vector3& a, const Vector3& b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_sub_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, 0);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, 0);
+	__m128 result = _mm_sub_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
 
 Vector3 operator*(const Vector3& a, const Vector3& b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_mul_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, 0);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, 0);
+	__m128 result = _mm_mul_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
 
 Vector3 operator/(const Vector3& a, const Vector3& b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_div_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, 0);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, 0);
+	__m128 result = _mm_div_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
 
 Vector3 operator+(const Vector3& a, float b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b, b, b, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_add_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, 0);
+	__m128 vb = _mm_set_ps(b, b, b, 0);
+	__m128 result = _mm_add_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
 
 Vector3 operator-(const Vector3& a, float b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b, b, b, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_sub_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, 0);
+	__m128 vb = _mm_set_ps(b, b, b, 0);
+	__m128 result = _mm_sub_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
 
 Vector3 operator*(const Vector3& a, float b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b, b, b, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_mul_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, 0);
+	__m128 vb = _mm_set_ps(b, b, b, 0);
+	__m128 result = _mm_mul_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
 
 Vector3 operator/(const Vector3& a, float b) {
-	__m256 va = _mm256_set_ps(a.x, a.y, a.z, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b, b, b, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_div_ps(va, vb);
+	__m128 va = _mm_set_ps(a.x, a.y, a.z, 0);
+	__m128 vb = _mm_set_ps(b, b, b, 0);
+	__m128 result = _mm_div_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
 
 Vector3 operator+(float a, const Vector3& b) {
-	__m256 va = _mm256_set_ps(a, a, a, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_add_ps(va, vb);
+	__m128 va = _mm_set_ps(a, a, a, 0);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, 0);
+	__m128 result = _mm_add_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
 
 Vector3 operator-(float a, const Vector3& b) {
-	__m256 va = _mm256_set_ps(a, a, a, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_sub_ps(va, vb);
+	__m128 va = _mm_set_ps(a, a, a, 0);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, 0);
+	__m128 result = _mm_sub_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
 
 Vector3 operator*(float a, const Vector3& b) {
-	__m256 va = _mm256_set_ps(a, a, a, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_mul_ps(va, vb);
+	__m128 va = _mm_set_ps(a, a, a, 0);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, 0);
+	__m128 result = _mm_mul_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
 
 Vector3 operator/(float a, const Vector3& b) {
-	__m256 va = _mm256_set_ps(a, a, a, 0, 0, 0, 0, 0);
-	__m256 vb = _mm256_set_ps(b.x, b.y, b.z, 0, 0, 0, 0, 0);
-	__m256 result = _mm256_div_ps(va, vb);
+	__m128 va = _mm_set_ps(a, a, a, 0);
+	__m128 vb = _mm_set_ps(b.x, b.y, b.z, 0);
+	__m128 result = _mm_div_ps(va, vb);
 	float* out = (float*)&result;
 	return Vector3{ out[7], out[6], out[5] };
 }
